54c8eaa90c97bf37f4eabd4c616c4376685b3197
Nomeleel <choonlay@outlook.com>
2020-03-27 23:41:40 +0800

    Import sudoku to main.

--- fine/main/main.py
+++ fine/main/main.py
@@ -1,5 +1,6 @@
 from config import appconfig
 from util import util
+from module.sudoku.sudoku import Sudoku
 from flask import Flask
 from flask import request
 import os
@@ -7,13 +8,11 @@ import os
 app = Flask(__name__)
 
 
+@app.route("/image", methods=['POST'])
 @app.route("/sudoku", methods=['POST'])
 def sudoku():
     image_file = request.files['image']
     if image_file and util.isImageByExtension(image_file.filename):
-        # Sudoku
-        # 1. save
-        #   image_file.save(os.path.join(file_path, util.uniqueName(image_file.filename)))
-        # 2. cv read
-        #   cv2.read() ???
+        Sudoku(image_file.read())
+        
         return '200'
--- fine/util/util.py
+++ fine/util/util.py
@@ -1,9 +1,9 @@
 from config import appconfig
 
 
-def uniqueName(self, base_name):
+def uniqueName(base_name):
     return time.strftime('%Y%m%d%H%M%S', time.localtime()) + base_name
 
 
-def isImageByExtension(self, image_name):
+def isImageByExtension(image_name):
     return '.' in image_name and image_name.rsplit('.', 1)[1].lower() in appconfig.IMAGE_EXTENSIONS
