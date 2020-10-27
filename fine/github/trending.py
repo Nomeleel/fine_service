620132d2fafe71d3aa4fe785bf59fe8a5d9c9c1f
Nomeleel <choonlay@outlook.com>
2020-10-27 23:04:44 +0800

    trending script add trim.

--- fine/github/trending.py
+++ fine/github/trending.py
@@ -1,6 +1,12 @@
 import requests
 from lxml import etree
 
+print('start')
+
+def trim(str_list):
+  str = ''.join(str_list)
+  return str.replace('\n', '')
+
 URL = 'https://github.com/trending'
 
 res = requests.get(URL)
@@ -10,10 +16,10 @@ html = etree.HTML(res.text)
 list = html.xpath('/html/body/div[4]/main/div[3]/div/div[2]/article')
 
 for e in list:
-  print(e.xpath('h1/a/span/text()'))
-  print(e.xpath('h1/a/text()'))
-  print(e.xpath('div[2]/span[1]/span[2]/text()'))
-  print(e.xpath('div[2]/a[1]/text()'))
-  print(e.xpath('div[2]/a[2]/text()'))
-  print(e.xpath('div[2]/span[3]/text()'))
+  print(trim(e.xpath('h1/a/span/text()')))
+  print(trim(e.xpath('h1/a/text()')))
+  print(trim(e.xpath('div[2]/span[1]/span[2]/text()')))
+  print(trim(e.xpath('div[2]/a[1]/text()')))
+  print(trim(e.xpath('div[2]/a[2]/text()')))
+  print(trim(e.xpath('div[2]/span[3]/text()')))
   print('----------------------------------------------------')
